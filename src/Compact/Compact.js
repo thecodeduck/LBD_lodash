dd7666100715bdfb7950c8868ec4fd4ee564a705
thecodeduck <thecodeduck@gmail.com>
2018-12-26 17:58:30 +0100

    Adding Compact check for undefined input

--- src/Compact/Compact.js
+++ src/Compact/Compact.js
@@ -1,4 +1,7 @@
 function Compact(arr) {
+	if (arr === undefined || !Array.isArray(arr)) {
+		return [];
+	}
 	const result = [];
 	const l = !arr.length ? 0 : arr.length;
 	for (let i = 0; i < l; i++) {
